18265b60be59e7dc4301f329dda943ec39fe044b
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-10-23 20:21:09 -0700

    0.0.1 release

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-extern crate firecore_dependencies as deps;
-
 pub mod music;
 pub mod sound;
 
--- src/sound.rs
+++ src/sound.rs
@@ -1,17 +1,14 @@
-use deps::str::TinyStr8;
+use tinystr::TinyStr8;
 
 pub type SoundId = TinyStr8;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Deserialize, serde::Serialize)]
 pub struct Sound {
-
     pub name: SoundId,
     pub variant: Option<u16>,
-
 }
 
 impl Sound {
-
     pub fn named(name: TinyStr8) -> Self {
         Self {
             name,
@@ -20,19 +17,15 @@ impl Sound {
     }
 
     pub fn variant(name: TinyStr8, variant: Option<u16>) -> Self {
-        Self {
-            name,
-            variant,
-        }
+        Self { name, variant }
     }
-
 }
 
 impl core::fmt::Display for Sound {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.variant {
             Some(variant) => write!(f, "{} #{}", self.name, variant),
-            None => core::fmt::Display::fmt(&self.name, f)
+            None => core::fmt::Display::fmt(&self.name, f),
         }
     }
 }
